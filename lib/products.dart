2b169d7bbc1f283b91a46212d6bb869efff58e5d
AlexandraPavel <72041403+AlexandraPavel@users.noreply.github.com>
2020-11-01 12:35:05 +0200

    Add feature
    ...

---
+++ lib/products.dart
@@ -0,0 +1,7 @@
+class Products{
+  int cod_de_bare;
+  String ingrediente, produs;
+  double pret;
+
+  Products(this.cod_de_bare, this.ingrediente, this.produs, this.pret);
+}
--- lib/shopping_cart.dart
+++ lib/shopping_cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'scan_screen.dart';
+import 'products.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class ShoppingScreen extends StatefulWidget{
   List<String> products;
@@ -12,22 +14,88 @@ class ShoppingScreen extends StatefulWidget{
 }
 
 class _ShoppingScreenState extends State<ShoppingScreen> {
-  List<String> products;
-  _ShoppingScreenState(this.products);
+  List<String> pr;
+  List<Products> postList = [];
+
+  _ShoppingScreenState(this.pr);
 
   @override
+  void initState() {
+    super.initState();
+
+    DatabaseReference postsRef = FirebaseDatabase.instance.reference().child(
+        "Produse");
+
+    postsRef.once().then((DataSnapshot snap) {
+      var KEYS = snap.value.keys;
+      var DATA = snap.value;
+
+      postList.clear();
+
+      for (var individualKey in KEYS) {
+        Text(
+            individualKey, style: TextStyle(color: Colors.black, fontSize: 10));
+        Products prod = new Products(
+          DATA[individualKey]['cod_de_bare'],
+          DATA[individualKey]['ingrediente'],
+          DATA[individualKey]['produs'],
+          DATA[individualKey]['pret'],
+        );
+        postList.add(prod);
+      }
+
+      setState(() {
+        print('Length : $postList.length');
+      });
+    });
+  }
+
+  double suma() {
+    double s = 0;
+    for (var i = 0; i < postList.length; i++) {
+      s += this.postList[i].pret;
+    }
+    return s;
+  }
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            return Container(
-              height: 50,
-              color: Colors.blue,
-              child: Center(child: Text('Entry ${products[index]}')),
-            );
-          }
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Cosul de cumparaturi", style: TextStyle(fontSize: 20),),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 50,
+            color: Colors.teal,
+            child: Center(child: Text(
+              '${postList[index].produs}.....${postList[index].pret} lei',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            ),
+
+          );
+        },
+        separatorBuilder: (contex, index) => Divider(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.teal,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            title: Container(height: 0.0),
+            icon: Icon(Icons.face_outlined, color: Colors.white),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.white,
+            title: Text('Total: ' + this.suma().toString() + ' lei ', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+            icon: Icon(Icons.shopping_cart, color: Colors.white),
+          ),
+        ],
       ),
     );
   }
